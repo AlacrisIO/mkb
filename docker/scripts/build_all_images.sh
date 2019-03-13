b5ded116167587f7555582b4e96404beeba4b539
vlebo <vedr@nlebo.com>
2019-03-13 11:38:51 +0100

    Renamed prereq image

--- docker/scripts/build_all_images.sh
+++ docker/scripts/build_all_images.sh
@@ -14,7 +14,7 @@ run() {
 }
 
 echo "Building build prerequisite image"
-run docker build -t gcr.io/legicash-demo-1950/legicash-demo/alacris-mkb-build-preprequisites:v1 -f ../containers/alacris_mkb_build_prerequisites/Dockerfile .
+run docker build -t gcr.io/legicash-demo-1950/legicash-demo/alacris-mkb-build-prerequisites:v1 -f ../containers/alacris_mkb_build_prerequisites/Dockerfile .
 
 echo "Building application images"
 run docker-compose -f ../docker-compose.yml build
